379bca46ac378d6ce5315ff3540b25f40502a675
Anastasia <anastasia.tuschina@yandex.ru>
2022-03-21 12:13:11 +0300

    добавляет компоненту posts с постами пользователя

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import Header from './components/header';
-import Main from './components/Main';
+import Header from './components/Header/header';
+import Main from './components/main-Content/Main';
 
 
 
