479c36572164d348d13b57728777ddcdcbf72125
Anastasia <anastasia.tuschina@yandex.ru>
2022-03-22 16:10:15 +0300

    добавляет пакет router

--- src/components/main-Content/profile/Profile.jsx
+++ src/components/main-Content/profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <div className={classes.content}>
             <div className={classes.content_banner}>
-                <img src='https://kald.secure2.footprint.net/fileadmin/data/seiten/produkte/badewannen/produktbilder/teaser-badewannen_smaller.png' />
+                <img src='https://kald.secure2.footprint.net/fileadmin/data/seiten/produkte/badewannen/produktbilder/teaser-badewannen_smaller.png' alt='dvd' />
             </div>
             <Posts/>
         </div>
